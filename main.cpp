201b2dcb80c87d93b400cc5b0c6086c943c02039
Edgar Rangel <rangeledgar136@gmail.com>
2025-02-17 22:19:22 -0800

    actions in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+[![Build C++](https://github.com/legendspy-1/HelloWorld/actions/workflows/actions.yml/badge.svg)](https://github.com/legendspy-1/HelloWorld/actions/workflows/actions.yml)
+
+
+
 int main(){
   std::cout<<"Hello World!\n";
   return 0;
